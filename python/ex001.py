d83686c27b3bcb5fe6a8ba81645314c507780def
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-08-13 13:19:59 -0300

    ex 1 e 2

--- python/ex001.py
+++ python/ex001.py
@@ -1,6 +1,6 @@
 nome = input("Qual é o seu nome: ")
 idade = input("Qual a sua idade: ")
-peso = input("Qual o seu peso: ") 
+peso = input("Qual o seu peso: ")
 
 print("\n")
 print(nome, "tem", idade, "anos e pesa", peso, "quilos.")
