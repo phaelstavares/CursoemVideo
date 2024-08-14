bc2ba2f8ae2e2f5ea25497039573a062fcddf2d4
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-08-14 15:13:17 -0300

    ex 5 e 6

---
+++ python/ex006.py
@@ -0,0 +1,6 @@
+import math
+
+num = int(input("Digite um número: "))
+print("O dobro de {} é {}.".format(num, (num * 2)))
+print("O triplo de {} é {}.".format(num, (num * 3)))
+print("A raiz quadrada de {} é igual a {}.".format(num, (math.sqrt(num))))
